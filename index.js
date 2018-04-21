61adff9a567a1a3c808b090e21b292ccbfbf27f3
ThakurBallary <thakurballary@github.com>
2018-04-21 21:20:52 +0530

    v1.0.0-beta

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class StatusColorPicker extends Component {
-    render() {
-        return(
-            <View style={styles.container}>
-                <Text>React Native Status Color Picker</Text>
-                <Text>Initial Release on April 22, 2018</Text>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        aligntItems: 'center',
-    }
-})
+import StatusColorPicker from './lib/StatusColorPicker';
+export default StatusColorPicker;
