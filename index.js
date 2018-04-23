8484a5dfa03ef977c2365ccfd330f26336438e51
ThakurBallary <thakurballary@gmail.com>
2018-04-23 14:49:50 +0530

    Export updated

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import ColorPicker from './lib/ColorPicker';
 import FullScreenColorStatus from './lib/FullScreenColorStatus';
 import StatusColorPicker from './lib/StatusColorPicker';
 
-export const ColorPicker;
-export const FullScreenColorStatus;
-export const StatusColorPicker;
+export { ColorPicker, FullScreenColorStatus, StatusColorPicker };
